c2204b1d81d8784a8d48178ba408b69222675b88
YuSungDuk <sungduk.yu@navercorp.com>
2020-07-31 18:33:35 +0900

    Add vpc region test code

--- ncloud/data_source_ncloud_regions_test.go
+++ ncloud/data_source_ncloud_regions_test.go
@@ -1,14 +1,13 @@
 package ncloud
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccDataSourceNcloudRegionsBasic(t *testing.T) {
-	t.Parallel()
-
-	resource.Test(t, resource.TestCase{
+func TestAccDataSourceNcloudRegionsClassic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
@@ -22,6 +21,27 @@ func TestAccDataSourceNcloudRegionsBasic(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceNcloudRegionsVPC(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudRegionsVPCConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDataSourceID("data.ncloud_regions.regions"),
+				),
+			},
+		},
+	})
+}
+
 var testAccDataSourceNcloudRegionsConfig = `
 data "ncloud_regions" "regions" {}
+`
+
+var testAccDataSourceNcloudRegionsVPCConfig = `
+data "ncloud_regions" "regions" {
+	platform_type = "vpc"
+}
 `
